511e13670b92354e1f9cbddfb95298ef78ee4812
SnoopyCodeX <extremeclasherph@gmail.com>
2024-01-18 03:59:36 +0800

    feat(admin/manage-promote-student): implement batch promote and individual promote

--- admin/views/manage-promote-student.php
+++ admin/views/manage-promote-student.php
@@ -7,19 +7,42 @@ require('../../auth/controller/auth.controller.php');
 
 // check if request is an ajax request
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    // fetch students
-    if (isset($_GET['year_level'])) {
-        $yearLevel = $dbCon->real_escape_string($_GET['year_level']);
+    $data = json_decode(file_get_contents('php://input'), true);
 
-        $query = "SELECT * FROM ap_userdetails WHERE year_level='$yearLevel' AND roles='student'";
+    // fetch students
+    if (isset($data['year_level'])) {
+        $yearLevel = $dbCon->real_escape_string($data['year_level']);
+
+        if ($yearLevel == 'All')
+            $query = "SELECT * FROM ap_userdetails WHERE roles='student'";
+        else
+            $query = "SELECT * FROM ap_userdetails WHERE roles='student' AND year_level='$yearLevel'";
+
         $result = $dbCon->query($query);
 
         if ($result->num_rows > 0) {
             $students = $result->fetch_all(MYSQLI_ASSOC);
+
+            // filter out selected students
+            if (isset($data['selected_student_ids']) && !empty($data['selected_student_ids'])) {
+                $selectedStudentIds = $data['selected_student_ids'];
+                $filteredStudents = [];
+
+                foreach ($students as $key => $student) {
+                    if (!in_array($student['id'], $selectedStudentIds)) {
+                        array_push($filteredStudents, $student);
+                    }
+                }
+
+                $students = $filteredStudents;
+            }
+
             echo json_encode($students);
         } else {
             echo json_encode([]);
         }
+
+        exit();
     }
 
     exit();
@@ -53,6 +76,64 @@ if (isset($_POST['promote-student'])) {
     }
 }
 
+// Batch promote students
+if (isset($_POST['batch-promote'])) {
+    $studentIds = json_decode($_POST['batch-promote-students']);
+
+    if (empty($studentIds)) {
+        $hasError = true;
+        $message = "No students selected";
+    } else {
+        // get all students
+        $query = "SELECT * FROM ap_userdetails WHERE roles='student'";
+        $result = $dbCon->query($query);
+
+        if ($result->num_rows > 0) {
+            $students = $result->fetch_all(MYSQLI_ASSOC);
+
+            // filter out selected students
+            $filteredStudents = [];
+
+            foreach ($students as $key => $student) {
+                if (in_array($student['id'], $studentIds)) {
+                    array_push($filteredStudents, $student);
+                }
+            }
+
+            $students = $filteredStudents;
+
+            // update year level
+            foreach ($students as $key => $student) {
+                // create a map of year level promotion, eg: 1st year -> 2nd year
+                $yearLevelMap = [
+                    '1st year' => '2nd Year',
+                    '2nd year' => '3rd Year',
+                    '3rd year' => '4th Year',
+                    '4th year' => '4th Year'
+                ];
+
+                // update year level
+                $yearLevel = $yearLevelMap[strtolower($student['year_level'])];
+                $query = "UPDATE ap_userdetails SET year_level='$yearLevel' WHERE id='{$student['id']}'";
+
+                if (!$dbCon->query($query)) {
+                    $hasError = true;
+                    $message = "An error occured while promoting <strong>{$student['firstName']} {$student['middleName']} {$student['lastName']}</strong> to <strong>$yearLevel</strong>";
+                    break;
+                }
+            }
+
+            if (!$hasError) {
+                $hasSuccess = true;
+                $message = "Students promoted successfully";
+            }
+        } else {
+            $hasError = true;
+            $message = "Error promoting students";
+        }
+    }
+}
+
 // pagination
 $limit = 10;
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
@@ -82,10 +163,28 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
                     <h1 class="text-[32px] font-bold">Promote Students</h1>
                 </div>
                 <div class="flex gap-4">
-                    <label class="btn" for="reset-academic">Promote Batch</label>
+                    <label class="btn" for="batch-promote-modal">Promote Batch</label>
                     <a href="./create/student.php" class="btn">Create</a>
                 </div>
             </div>
+
+            <?php if ($hasError) { ?>
+                <div role="alert" class="alert alert-error mb-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <span><?= $message ?></span>
+                </div>
+            <?php } ?>
+
+            <?php if ($hasSuccess) { ?>
+                <div role="alert" class="alert alert-success mb-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <span><?= $message ?></span>
+                </div>
+            <?php } ?>
 
             <!-- Table Content -->
             <div class="overflow-x-hidden border border-gray-300 rounded-md" style="height: calc(100vh - 250px)">
@@ -98,6 +197,7 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
                             <td class="bg-slate-500 text-white">Email</td>
                             <td class="bg-slate-500 text-white">Gender</td>
                             <td class="bg-slate-500 text-white">Contact</td>
+                            <td class="bg-slate-500 text-white">Year Level</td>
                             <td class="bg-slate-500 text-white text-center">Action</td>
                         </tr>
                     </thead>
@@ -115,6 +215,7 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
                                         <td>{$row['email']}</td>
                                         <td>" . ucfirst($row['gender']) . "</td>
                                         <td>{$row['contact']}</td>
+                                        <td>" . strtolower($row['year_level']) . "</td>
                                         <td>
                                             <div class='flex gap-2 justify-center items-center'>
                                                 <label for='view-student-{$row['id']}' class='btn btn-sm'>View</label>
@@ -250,10 +351,10 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
                             <span class="font-bold text-[18px]">Promote to year level</span>
                             <select class="select select-bordered" name="year_level" required>
                                 <option value="" selected disabled>Select year</option>
-                                <?php if ($row['year_level'] != '1st year') { ?> <option vaue="1st Year">1st Year</option> <?php } ?>
-                                <?php if ($row['year_level'] != '2nd year') { ?> <option vaue="2nd Year">2nd Year</option> <?php } ?>
-                                <?php if ($row['year_level'] != '3rd year') { ?> <option vaue="3rd Year">3rd Year</option> <?php } ?>
-                                <?php if ($row['year_level'] != '4th year') { ?> <option vaue="4th Year">4th Year</option> <?php } ?>
+                                <?php if (strtolower($row['year_level']) != '1st year') { ?> <option vaue="1st Year">1st Year</option> <?php } ?>
+                                <?php if (strtolower($row['year_level']) != '2nd year') { ?> <option vaue="2nd Year">2nd Year</option> <?php } ?>
+                                <?php if (strtolower($row['year_level']) != '3rd year') { ?> <option vaue="3rd Year">3rd Year</option> <?php } ?>
+                                <?php if (strtolower($row['year_level']) != '4th year') { ?> <option vaue="4th Year">4th Year</option> <?php } ?>
                             </select>
                         </label>
 
@@ -272,10 +373,10 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
 </main>
 
 <!-- Batch promote modal -->
-<input type="checkbox" id="reset-academic" class="modal-toggle" />
+<input type="checkbox" id="batch-promote-modal" class="modal-toggle" />
 <div class="modal" role="dialog">
     <div class="modal-box">
-        <form id="batch-promote-form" method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
+        <form id="batch-promote-form" method="post" action="<?= $_SERVER['PHP_SELF'] ?>" id="batch-promote-form">
             <label class="flex flex-col gap-2">
                 <div class="flex justify-between items-center">
                     <span class="font-bold text-[18px]">Students</span>
@@ -284,6 +385,7 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
                         <select class="select select-bordered select-sm" id="batch-year-level-selector">
                             <!--Display all the Year level here-->
                             <option value="" selected disabled>Select Year level</option>
+                            <option value="All">All</option>
                             <option value="1st Year">1st Year</option>
                             <option value="2nd Year">2nd Year</option>
                             <option value="3rd Year">3rd Year</option>
@@ -299,22 +401,24 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
 
                     <?php $students = $dbCon->query($query); ?>
                     <?php while ($student = $students->fetch_assoc()) { ?>
-                        <div class="h-[48px] flex gap-4 justify-start px-4 items-center  gap-4 border border-gray-400 rounded-[5px]">
+                        <div class="h-[48px] flex gap-4 justify-start px-4 items-center border border-gray-400 rounded-[5px]">
                             <input type="checkbox" class="checkbox checkbox-sm" />
-                            <span><?= $student['firstName'] ?> <?= $student['middleName'] ?> <?= $student['lastName'] ?></span>
+                            <span data-studentId="<?= $student['id'] ?>"><?= $student['firstName'] ?> <?= $student['middleName'] ?> <?= $student['lastName'] ?></span>
                         </div>
                     <?php } ?>
 
                 </div>
             </label>
 
+            <input type="hidden" name="batch-promote-students" id="batch-promote-students" />
+
             <div class="flex gap-2 flex-col mt-4">
-                <button class="btn btn-success w-full">Submit</button>
-                <label class="btn w-full" for="reset-academic">Close</label>
+                <button class="btn btn-success w-full" name="batch-promote">Promote</button>
+                <label class="btn w-full" for="batch-promote-modal">Close</label>
             </div>
         </form>
     </div>
-    <label class="modal-backdrop" for="reset-academic">Close</label>
+    <label class="modal-backdrop" for="batch-promote-modal">Close</label>
 </div>
 
 <script>
@@ -333,12 +437,28 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
             });
         });
 
+        // Batch promote form
+        const batchPromoteForm = document.querySelector('#batch-promote-form');
+        batchPromoteForm.addEventListener('submit', (e) => {
+            const selectedStudents = Array.from(body.querySelectorAll("input[type='checkbox']:checked"));
+            const selectedStudentIds = selectedStudents.map(student => student.parentElement.querySelector('span').dataset.studentid);
+
+            document.querySelector('#batch-promote-students').value = JSON.stringify(selectedStudentIds);
+        });
+
         // Year level filter dropdown
         select.addEventListener('change', (e) => {
+            const selectedStudents = Array.from(body.querySelectorAll("input[type='checkbox']:checked"));
+            const selectedStudentIds = selectedStudents.map(student => student.parentElement.querySelector('span').dataset.studentid);
             const yearLevel = e.target.value;
 
             // Fetch all students with the same year level
-            fetch(`<?= $_SERVER['PHP_SELF'] ?>?year_level=${encodeURIComponent(yearLevel)}`, {
+            fetch(`<?= $_SERVER['PHP_SELF'] ?>`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        year_level: yearLevel,
+                        selected_student_ids: selectedStudentIds
+                    }),
                     headers: {
                         'X-Requested-With': 'XMLHttpRequest',
                         'content-type': 'application/json'
@@ -346,18 +466,23 @@ $query = "SELECT * FROM ap_userdetails WHERE roles='student' LIMIT $start, $limi
                 })
                 .then(res => res.json())
                 .then(data => {
-                    // Clear the body
-                    body.innerHTML = "";
+                    // get all unselected students from studentContainer
+                    const unselectedStudents = Array.from(body.querySelectorAll("input[type='checkbox']:not(:checked)"));
+
+                    // remove all unselected students from studentContainer
+                    unselectedStudents.forEach(student => student.parentElement.remove());
 
                     // Append the students
                     data.forEach(student => {
-                        body.innerHTML += `
-                    <div class="h-[48px] flex gap-4 justify-start px-4 items-center  gap-4 border border-gray-400 rounded-[5px]">
-                        <input type="checkbox" class="checkbox checkbox-sm" />
-                        <span>${student.firstName} ${student.middleName} ${student.lastName}</span>
-                    </div>
-                `;
-                    });
+                        const studentDiv = document.createElement("div");
+                        studentDiv.classList.add("h-[48px]", "flex", "gap-4", "justify-start", "px-4", "items-center", "gap-4", "border", "border-gray-400", "rounded-[5px]");
+                        studentDiv.innerHTML = `
+                            <input type="checkbox" class="checkbox checkbox-sm" />
+                            <span data-studentId="${student.id}">${student.firstName} ${student.middleName} ${student.lastName}</span>
+                        `;
+
+                        body.appendChild(studentDiv);
+                    })
                 });
         });
     });
