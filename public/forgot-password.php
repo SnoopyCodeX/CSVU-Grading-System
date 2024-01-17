b62a1942863dbf36bf86325c0680659cdaef6f35
SnoopyCodeX <extremeclasherph@gmail.com>
2024-01-17 17:52:50 +0800

    refactor(public/forgot-password): refactor forgot password page ui

--- public/forgot-password.php
+++ public/forgot-password.php
@@ -1,16 +1,13 @@
 <?php
-require_once("./components/header.php");
+require_once("../components/header.php");
 ?>
 <main class="w-screen h-screen border border-black p-4 overflow-hidden">
-    <?php
-        require_once("./components/auth-header.php")
-    ?>
     <div class="h-full flex justify-center items-center flex-col h-full">
         <div class="w-[500px] h-[500px]">
-                <!-- Header -->
+            <!-- Header -->
             <div class="flex flex-col justify-center items-center gap-4 mb-8">
                 <h1 class="text-[32px] font-bold">Forgot Password</h1>
-                <span class="text-base text-center">Oops, locked out? No worries! If you've forgotten your password, Enter your email' below. We'll guide you through a quick verification process to ensure account security.</span>
+                <span class="text-base text-center">Oops, locked out? No worries! If you've forgotten your password, Enter your email address below. We'll guide you through a quick verification process to ensure account security.</span>
             </div>
 
             <!-- Form -->
@@ -21,10 +18,10 @@ require_once("./components/header.php");
                         <span>Email</span>
                         <input type="email" class="input input-bordered input-md" placeholder="Enter your Email" />
                     </label>
-                    
+
                     <!-- Break -->
                     <span class="border border-black my-2"></span>
-                    
+
                     <!-- Button -->
                     <button class="btn w-full">Verfiy</button>
                 </div>
