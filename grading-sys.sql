c41d8d15b799210072da1cd3bab7ec609ea3a9e7
SnoopyCodeX <extremeclasherph@gmail.com>
2024-01-18 13:18:54 +0800

    chore(db): update sql dump

--- grading-sys.sql
+++ grading-sys.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2024 at 09:45 PM
+-- Generation Time: Jan 18, 2024 at 06:18 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -39,7 +39,7 @@ CREATE TABLE `ap_courses` (
 
 INSERT INTO `ap_courses` (`id`, `course`, `course_code`) VALUES
 (2, 'test', 123),
-(3, 'another course', 231);
+(3, 'another course', 2316);
 
 -- --------------------------------------------------------
 
@@ -58,7 +58,9 @@ CREATE TABLE `ap_school_year` (
 
 INSERT INTO `ap_school_year` (`id`, `school_year`) VALUES
 (1, '2022 - 2023'),
-(2, '2023 - 2024');
+(2, '2023 - 2024'),
+(3, '2024 - 2025'),
+(4, '2025 - 2026');
 
 -- --------------------------------------------------------
 
@@ -82,8 +84,7 @@ CREATE TABLE `ap_sections` (
 --
 
 INSERT INTO `ap_sections` (`id`, `name`, `subject`, `school_year`, `term`, `year_level`, `course`, `instructor`) VALUES
-(6, 'Section Kamote', 3, 2, '1st Sem', '1st Year', 2, 21),
-(8, 'Section Borta', 3, 2, '1st Sem', '3rd Year', 3, 21);
+(10, 'Section Sampaguita', 4, 2, '1st Sem', '1st Year', 2, 23);
 
 -- --------------------------------------------------------
 
@@ -102,9 +103,8 @@ CREATE TABLE `ap_section_students` (
 --
 
 INSERT INTO `ap_section_students` (`id`, `section_id`, `student_id`) VALUES
-(11, 6, 1),
-(12, 6, 19),
-(13, 8, 4);
+(17, 10, 24),
+(18, 10, 25);
 
 -- --------------------------------------------------------
 
@@ -156,15 +156,16 @@ CREATE TABLE `ap_userdetails` (
 --
 
 INSERT INTO `ap_userdetails` (`id`, `firstName`, `middleName`, `lastName`, `email`, `password`, `gender`, `contact`, `birthday`, `year_level`, `roles`, `sid`) VALUES
-(1, 'Albert', 'Pogi', 'Winkler', 'asklahanov@gmail.com', '$6$Crypt$1qVc0jGeKh7JeEWTUq5Nfadlm9iXqTnb6kSMA0M2B17o6sJh2CG/I9AQytLtkFfpIkxlLZRCtP.FR2.gWiOxG/', 'male', 2147483647, '1900-01-01', '1st year', 'student', '123123123'),
+(1, 'Albert', 'Pogi', 'Winkler', 'asklahanov@gmail.com', '$6$Crypt$1qVc0jGeKh7JeEWTUq5Nfadlm9iXqTnb6kSMA0M2B17o6sJh2CG/I9AQytLtkFfpIkxlLZRCtP.FR2.gWiOxG/', 'male', 2147483647, '1900-01-01', '3rd Year', 'student', '123123123'),
 (2, 'Albert', 'Pogi', 'Winkler', 'askalkaba73@gmail.com', '$6$Crypt$A/jDa6/VXgega4JS.fwcrnSPsSGc8iYDamgc9d0eVLyMU725Br1vK00ffObZUSbr/Enjrgh2S40phWnAcb.2w1', 'female', 2147483647, '1990-01-01', '', 'admin', '432432432'),
 (3, 'Criztian ', 'Mitra', 'Pogi', 'criztianpog123@gmail.com', '$6$Crypt$.IPCVAofnRERoq/EF59k00yPGSCWboFqBr/evkTVdPrpx.TjOUlI0Mi/.3jJYjPdsL7A3MZWpjAgu3DkjHmAj0', 'female', 2147483647, '1990-01-01', '', 'admin', '283828382'),
-(4, 'Criztian', 'Pogi', 'Tuplano', 'asukalkaba@gmail.com', '$6$Crypt$.IPCVAofnRERoq/EF59k00yPGSCWboFqBr/evkTVdPrpx.TjOUlI0Mi/.3jJYjPdsL7A3MZWpjAgu3DkjHmAj0', 'male', 92874747, '1900-01-01', '3rd year', 'student', '1231231232'),
-(11, 'John Roy', 'Middlename', 'Lapida', 'johnroy062102calimlim@gmail.com', '$6$Crypt$Svhm9rLEJJ6E99h1C2wo5Sdjz4PhjfD3w93g7EkIfrIB15bnY5Os5sdaYDCIGYeqG1JrQzM6A2EJpCJHIsSNd0', 'female', 2147483647, '1990-01-01', '', 'admin', '12345555'),
+(4, 'Criztian', 'Pogi', 'Tuplano', 'asukalkaba@gmail.com', '$6$Crypt$.IPCVAofnRERoq/EF59k00yPGSCWboFqBr/evkTVdPrpx.TjOUlI0Mi/.3jJYjPdsL7A3MZWpjAgu3DkjHmAj0', 'male', 92874747, '1900-01-01', '4th Year', 'student', '1231231232'),
+(11, 'John Roy', '', 'Lapida', 'johnroy062102calimlim@gmail.com', '$6$Crypt$Svhm9rLEJJ6E99h1C2wo5Sdjz4PhjfD3w93g7EkIfrIB15bnY5Os5sdaYDCIGYeqG1JrQzM6A2EJpCJHIsSNd0', 'female', 2147483647, '1990-01-01', '', 'admin', '12345555'),
 (12, 'Albert', 'Pogi', 'Aklan', 'contact@hax4life.org', '$6$Crypt$CAJbLqrN2i959BY9fYtFPc3nLNhHKkAqQVbbpCdez3H/OkFiLEq.JchLmxMmhCaxm/ekMcs1On5oCsfJLxzKR1', 'female', 2147483647, '1990-01-01', '', 'admin', '32432432'),
-(19, 'Albert', 'Kupal', 'raulo', 'albertkupal@gmail.com', '$6$Crypt$EsHmqkQvmwperb8BCLW071h5wYsb/nu6wrJospA4bBOedphtupEzPs7.Xj6G2O.jrZg6VDrzA7zy0PHPTkcOG1', 'female', 2147483647, '1900-01-01', '1st year', 'student', '780870948572'),
-(21, 'another', 'test', 'instructor', 'another@gmail.com', '$6$Crypt$BmkKx1B9DKub8BG07ZzX0jAEbXJQyPKW620nYoVBVSR4uMSCIdkZpEn08hIihe98JO7h5X0rXqcYecD1uroyj0', 'female', 2147483647, '1900-01-01', NULL, 'instructor', NULL),
-(22, 'Test', 'Admin', 'Admin', 'testadmin@gmail.com', '$6$Crypt$r4LPGK8Cg2kyNTyMG4ti3V/Oo2Kog1dYktxrleQdlo2cinQrxZNz97t3Boo6CMsQuJAtXLginJajkD.X.c0EU/', 'male', 2147483647, '1900-01-01', NULL, 'admin', NULL);
+(19, 'Albert', '', 'Pogi', 'albertpogi123@gmail.com', '$6$Crypt$EsHmqkQvmwperb8BCLW071h5wYsb/nu6wrJospA4bBOedphtupEzPs7.Xj6G2O.jrZg6VDrzA7zy0PHPTkcOG1', 'male', 2147483647, '1900-01-01', '3rd Year', 'student', '780870948572'),
+(23, 'Albert', '', 'Winkler', 'albert@instructor.com', '$6$Crypt$PZeqbcn2b92pYsPbMIaJM6JIORhx4WCJm0GPWHc/pXnxAmaRdfyhTEZSCMOBipJpMFfto5x75FtH3LgWghiFS0', 'male', 2147483647, '1900-01-01', NULL, 'instructor', NULL),
+(24, 'Louise', '', 'Rouxe', 'louiserouxe@gmail.com', '$6$Crypt$6VtryvO3DqQgz32i5hEQsNs43DwdTbEmlcxNe1816FMWVBWrMYb0kvb.JWz4/YN.fEXMEH2b8JqgBsVHQYVIk1', 'female', 2147483647, '1900-01-01', '1st year', 'student', '5423413'),
+(25, 'Rex', '', 'Rider', 'rexrider@gmail.com', '$6$Crypt$AdOgW1ELa7yGkykBpgkQPrpjo1iPyrV06NZvokMNvmfPCWVZJe1HXueA7m.IZ7gjSAfPJ5p6By7X4C06e2s6T.', 'male', 2147483647, '1900-01-01', '1st year', 'student', '0987687');
 
 --
 -- Indexes for dumped tables
@@ -229,19 +230,19 @@ ALTER TABLE `ap_courses`
 -- AUTO_INCREMENT for table `ap_school_year`
 --
 ALTER TABLE `ap_school_year`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `ap_sections`
 --
 ALTER TABLE `ap_sections`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `ap_section_students`
 --
 ALTER TABLE `ap_section_students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `ap_subjects`
@@ -253,7 +254,7 @@ ALTER TABLE `ap_subjects`
 -- AUTO_INCREMENT for table `ap_userdetails`
 --
 ALTER TABLE `ap_userdetails`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Constraints for dumped tables
